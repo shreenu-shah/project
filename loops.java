890df70b16343e4cba8f58cd5fe902ca19b10d39
Shreenu Shah <shreenushah19@gmail.com>
2026-08-29 01:23:38 +0530

    Print numbers from 1 to 5

--- loops.java
+++ loops.java
@@ -1,24 +1,78 @@
 import java.util.Scanner;
 public class loops {
     public static void main(String args[]){
-        Scanner Ac =new Scanner(System.in);
-        System.out.println("Enter your Age: ");
-        int Age= Ac.nextInt();
-        System.out.println(Age);
-    if(Age<=12){
-        System.out.println("YOu area child");
-    }
-    else if(Age>=12 && Age<18){
-        System.out.println("YOu are a teenager");
-    }
-    else if(Age>18)
-    {
-    System.out.println("You are a adult now");
-}
-else{
-    System.out.println("invalid age");
+//         Scanner Ac =new Scanner(System.in);
+//         System.out.println("Enter your Age: ");
+//         int Age= Ac.nextInt();
+//         System.out.println(Age);
+//     if(Age<=12){
+//         System.out.println("YOu area child");
+//     }
+//     else if(Age>=12 && Age<18){
+//         System.out.println("YOu are a teenager");
+//     }
+//     else if(Age>18)
+//     {
+//     System.out.println("You are a adult now");
+// }
+// else{
+//     System.out.println("invalid age");
+// }
+// //Even and divisble by three
+// Scanner Even = new Scanner(System.in);
+// System.out.println("Enter your number: ");
+// int number=Even.nextInt();
+// if(number%2==0 && number%3==0){
+//     System.out.println("Number is Even and divisible by 3");
+//     }
+//     else{
+//         System.out.println("Number is not Even and  not divisible by 3");
+//     }
+// divisible by 3 or 5
+// Scanner Sc = new Scanner(System.in);
+// System.out.println("Enter your number: ");
+// int Num = Sc.nextInt();
+// if(Num%3==0 || Num %5==0){
+//     System.out.println("Number is divisible by 3 or 5");
+// }
+// else{
+//     System.out.println("Number is not divisible by 3 or 5");
+// }
+// Switch
+// Scanner Sc = new Scanner(System.in);
+// System.out.println("Enter Number: ");
+// int Num = Sc.nextInt();
+// switch(Num){
+//     case 1:
+//     System.out.println("Monday");
+//     break;
+//     case 2:
+//     System.out.println("Today is tuesday");
+//     break;
+//     case 3:
+//         System.out.println("Today is wednesday");
+//     break;
+//     case 4:
+//         System.out.println("Today is Thursday");
+//     break;
+//     case 5:
+//     System.out.println("Today is Friday");
+//     break;
+//     case 6:
+//     System.out.println("Today is Saturday");
+//     break;
+//     default:
+//     System.out.println("Invalid Day");
+// }
+Scanner pc= new Scanner(System.in);
+System.out.println("Enter your number:");
+int N = pc.nextInt();
+int Number=1;
+while(Number<=N){
+    System.out.println(Number);
+    Number++;
 }
 
-    }
+} 
     
 }
